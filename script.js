02c73d0610a7f1ad82263e15ac4a7e0905c6128b
Shreyas Panicker <shreyas.panicker@kottsoftware.com>
2025-03-14 18:29:21 +0530

    Menu and Sub Menu portion completed

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
-import menuData from "./data/menu.json";
+import menuData from "./data/menu.json" with { type: 'json' }
 const dropdownMenu = document.querySelector(".dropdown");
 
-document.querySelector(".dropdown-button").addEventListener("click", (e) => {
+
+const createMenuItems = (target) => {
+  let menuContent = menuData[target.outerText.toLowerCase()]
+  if (menuContent) {
+    if (target.querySelector('.menu-list')) {
+      document.querySelector(".menu-list").style.display = "block"
+    } else {
+      const contentTexts = Object.keys(menuContent)
+      const content = document.createElement("div");
+      content.className = 'menu-list'
+      contentTexts.forEach(contentText => {
+        const listItem = document.createElement('div')
+        listItem.className = 'list-item'
+        listItem.textContent = contentText;
+        listItem.addEventListener('mouseenter', (e) => {
+          if (target.querySelector('.sub-menu-list')) {
+            target.removeChild(target.querySelector(".sub-menu-list"))
+          }
+          const subContents = menuContent[e.target.outerText];
+          const subContentElement = document.createElement("div");
+          subContentElement.className = 'sub-menu-list'
+          subContents.forEach(each => {
+            const subListItem = document.createElement('div')
+            subListItem.className = 'sub-list-item';
+            subListItem.textContent = each.name;
+            subContentElement.appendChild(subListItem)
+          })
+          target.appendChild(subContentElement)
+
+        })
+        content.appendChild(listItem)
+      })
+      target.appendChild(content)
+      document.querySelector(".menu-list").style.display = "block"
+    }
+  }
+
+}
+
+document.querySelector(".dropdown-button").addEventListener("click", e => {
   e.stopPropagation();
   dropdownMenu.classList.toggle("show");
   document.querySelector(".dropdown-button").style.display = "none";
@@ -16,25 +55,28 @@ document.addEventListener("click", () => {
   }
 });
 
-document.querySelector(".nav li").addEventListener("mouseover", () => {
-  document.querySelector(".menu-contents").style.display = "grid";
+const allMenuItems = Array.from(document.querySelectorAll("header .nav li"))
+allMenuItems.forEach(menuItem => {
+  menuItem.addEventListener("mouseenter", (e) => {
+    createMenuItems(e.target);
+  });
+})
+
+document.querySelector('header').addEventListener("mouseleave", () => {
+  document.querySelector(".menu-list").style.display = "none";
+  document.querySelector(".sub-menu-list").style.display = "none";
 });
 
-document.querySelector(".nav li").addEventListener("mouseout", () => {
-  document.querySelector(".menu-contents").style.display = "none";
-});
+
 
 window.addEventListener(
   "resize",
-  (event) => {
+  event => {
     if (event.currentTarget.innerWidth > 620) {
       document.querySelector(".dropdown-button").style.display = "none";
       document.querySelector(".close-button").style.display = "none";
       dropdownMenu.classList.remove("show");
-    } else if (
-      event.currentTarget.innerWidth < 620 &&
-      !dropdownMenu.classList.contains("show")
-    ) {
+    } else if (event.currentTarget.innerWidth < 620 && !dropdownMenu.classList.contains("show")) {
       document.querySelector(".dropdown-button").style.display = "block";
       document.querySelector(".close-button").style.display = "none";
     }
@@ -46,7 +88,7 @@ const indicators = document.querySelectorAll(".slider .indicators .indicator");
 const arrowPrev = document.querySelector(".slider .arrows .arrow-prev");
 const arrowNext = document.querySelector(".slider .arrows .arrow-next");
 
-const handleIndicatorClick = (event) => {
+const handleIndicatorClick = event => {
   const indicator = event.target;
   if (!isActive(indicator)) {
     removeActive();
@@ -55,53 +97,45 @@ const handleIndicatorClick = (event) => {
   }
 };
 
-const handlePrevArrowClick = (event) => {
+const handlePrevArrowClick = event => {
   let activeSlide = 0;
   let newActiveSlide = indicators.length;
   let ready = false;
 
-  indicators.forEach((indicator) => {
+  indicators.forEach(indicator => {
     if (isActive(indicator) && !ready) {
       activeSlide = indicator.dataset.slide;
       if (activeSlide !== "1") {
         newActiveSlide = parseInt(activeSlide) - 1;
       }
       removeActive();
-      addActive(
-        document.querySelector(
-          `.slider .indicators [data-slide='${newActiveSlide}']`
-        )
-      );
+      addActive(document.querySelector(`.slider .indicators [data-slide='${newActiveSlide}']`));
       showSlide(newActiveSlide);
       ready = true;
     }
   });
 };
 
-const handleNextArrowClick = (event) => {
+const handleNextArrowClick = event => {
   let activeSlide = 0;
   let newActiveSlide = 1;
   let ready = false;
 
-  indicators.forEach((indicator) => {
+  indicators.forEach(indicator => {
     if (isActive(indicator) && !ready) {
       activeSlide = indicator.dataset.slide;
       if (activeSlide !== indicators.length.toString()) {
         newActiveSlide = parseInt(activeSlide) + 1;
       }
       removeActive();
-      addActive(
-        document.querySelector(
-          `.slider .indicators [data-slide='${newActiveSlide}']`
-        )
-      );
+      addActive(document.querySelector(`.slider .indicators [data-slide='${newActiveSlide}']`));
       showSlide(newActiveSlide);
       ready = true;
     }
   });
 };
 
-indicators.forEach((indicator) => {
+indicators.forEach(indicator => {
   indicator.addEventListener("click", handleIndicatorClick);
 });
 
@@ -113,7 +147,7 @@ function isActive(indicator) {
 }
 
 function removeActive() {
-  document.querySelectorAll(".slider .indicators [active]").forEach((item) => {
+  document.querySelectorAll(".slider .indicators [active]").forEach(item => {
     item.removeAttribute("active");
   });
 }
